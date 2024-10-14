b81bb4b8f40fb89c8d65f2edc814bc9bccad72ec
MashukovI <ivan03.00@mail.ru>
2024-10-14 12:22:26 +0500

    start load sum100

--- NuRePrCalc/Form1.cs
+++ NuRePrCalc/Form1.cs
@@ -10,7 +10,7 @@ namespace NuRePrCalc
 {
     public partial class Form1 : Form
     {
-
+        string filePath = "D:\\Kursovaya\\Kursach\\NuRePrCalc\\ExcelDB.xlsx";
         private Dictionary<string, string> cellMappings = new Dictionary<string, string>
         {
             { "Шахматное", "D8" },
@@ -40,7 +40,7 @@ namespace NuRePrCalc
         public Form1()
         {
 
-
+            this.Load += new EventHandler(Form1_Load);
             InitializeComponent();
 
             for (int i = 10; i <= 90; i += 10)
@@ -55,6 +55,100 @@ namespace NuRePrCalc
             comboBoxNuCells.SelectedIndex = 0;
 
             LoadImage();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadInitialValues(); // Загружаем данные из Excel и отображаем в TextBox
+        }
+
+        private void LoadInitialValues()
+        {
+
+            string selectedDescription = comboBoxNuCells.SelectedItem.ToString();
+
+            if (System.IO.File.Exists(filePath))
+            {
+                Type excelType = Type.GetTypeFromProgID("Excel.Application");
+                dynamic excelApp = Activator.CreateInstance(excelType);
+
+                try
+                {
+                    excelApp.Visible = false;
+                    excelApp.DisplayAlerts = false;
+
+                    dynamic workbook = excelApp.Workbooks.Open(filePath);
+                    dynamic worksheet = workbook.Sheets[1];
+
+                    // Загружаем данные из Excel в TextBox при старте
+                    textBoxT.Text = worksheet.Cells[3, 1].Value.ToString();
+                    textBoxW.Text = worksheet.Cells[3, 6].Value.ToString();
+                    textBoxD.Text = worksheet.Cells[3, 7].Value.ToString();
+                    comboBoxA.Text = worksheet.Cells[3, 11].Value.ToString();
+                    textBoxCO2.Text = worksheet.Cells[3, 2].Value.ToString();
+                    textBoxH2O.Text = worksheet.Cells[3, 3].Value.ToString();
+                    textBoxN2.Text = worksheet.Cells[3, 4].Value.ToString();
+                    textBoxO2.Text = worksheet.Cells[3, 5].Value.ToString();
+
+                    textBoxPr.Text = worksheet.Cells[43, 6].Value.ToString(); // F43
+                    textBoxRe.Text = worksheet.Cells[8, 3].Value.ToString();  // C8
+
+                    // Использование сопоставления для получения адреса выбранной ячейки
+                    string cellAddress = cellMappings[selectedDescription];
+                    textBoxNu.Text = worksheet.Range[cellAddress].Value.ToString();
+
+                    string cellAddress2 = cellMappings2[selectedDescription];
+                    textBoxV.Text = worksheet.Range[cellAddress2].Value.ToString();
+
+                    textBoxAA.Text = worksheet.Cells[3, 13].Value.ToString(); // M3
+
+                    workbook.Application.Calculate();
+                    workbook.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+                finally
+                {
+                    Marshal.ReleaseComObject(excelApp);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден: " + filePath);
+            }
+        }
+
+        private bool ValidateSum()
+        {
+            try
+            {
+                // Преобразуем значения из текстовых полей в числа
+                double co2 = double.Parse(textBoxCO2.Text);
+                double h2o = double.Parse(textBoxH2O.Text);
+                double n2 = double.Parse(textBoxN2.Text);
+                double o2 = double.Parse(textBoxO2.Text);
+
+                // Считаем сумму
+                double sum = co2 + h2o + n2 + o2;
+
+                // Проверяем, равна ли сумма 100
+                if (sum != 100)
+                {
+                    // Если не равна, выводим ошибку
+                    MessageBox.Show($"Ошибка: Сумма должна быть равна 100! Сейчас сумма = {sum}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false; // Возвращаем false, если сумма не равна 100
+                }
+
+                return true; // Возвращаем true, если сумма верна
+            }
+            catch (FormatException)
+            {
+                // Если введены некорректные данные (не числа)
+                MessageBox.Show("Ошибка: Введите числовые значения!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
         }
 
         private void comboBoxNuCells_SelectedIndexChanged(object sender, EventArgs e)
@@ -74,7 +168,7 @@ namespace NuRePrCalc
 
         private void LoadExcelDataAndPlot()
         {
-            string filePath = "D:\\Kursovaya\\NuRePrCalc\\ExcelDB.xlsx";
+            
 
             using (var workbook = new XLWorkbook(filePath))
             {
@@ -147,7 +241,11 @@ namespace NuRePrCalc
 
         private void button11_Click(object sender, EventArgs e)
         {
-            string filePath = "D:\\Kursovaya\\NuRePrCalc\\ExcelDB.xlsx";
+
+            if (!ValidateSum())
+            {
+                return; // Если сумма неправильная, не продолжаем расчет
+            }
 
             string selectedDescription = comboBoxNuCells.SelectedItem.ToString();
 
